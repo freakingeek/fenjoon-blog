bc2eb0806d39edc3c4b7c658652d56794888bf76
Yasin Silavi <ys.silavi@gmail.com>
2025-04-07 03:50:05 +0330

    feat: implement desktop header WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,46 @@
+import Container from "@/components/Container";
+import Link from "next/link";
+
 export default function HomePage() {
-  return <section>به نام خدای حسین!</section>;
+  return (
+    <Container>
+      <header className="flex justify-between items-center">
+        <h1 className="text-xl font-extrabold">فنجون</h1>
+
+        <nav>
+          <ul className="flex gap-x-8">
+            <li>
+              <Link href="/" className="hover:underline hover:text-primary">
+                خانه
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                href="/about"
+                className="hover:underline hover:text-primary"
+              >
+                درباره ما
+              </Link>
+            </li>
+
+            <li>
+              <Link href="/blog" className="hover:underline hover:text-primary">
+                وبلاگ
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                href="https://app.fnjo.ir/"
+                className="hover:underline hover:text-primary"
+              >
+                ورود به اپلیکیشن
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </Container>
+  );
 }
